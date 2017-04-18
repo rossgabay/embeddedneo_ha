e5baa386e2df6115388acacb4a63a6abc8a6901b
rossgabay <rossgabay@Ross-Gabays-MacBook-Pro.local>
2017-04-18 12:31:54 -0400

    initial commit

---
+++ src/main/java/com/rgabay/embedded_ha/App.java
@@ -0,0 +1,13 @@
+package com.rgabay.embedded_ha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
